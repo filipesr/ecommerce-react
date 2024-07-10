144aa1f0b1665082b78d2bd8bbeb8aa0ac4fe382
Filipe Rezende <fsr.trabalho@gmail.com>
2024-07-10 18:02:15 -0400

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBsS40br64Ed2SWggm6kH3CX95GGW0wRMg",
-  authDomain: "ignite-store-1719855635609.firebaseapp.com",
-  projectId: "ignite-store-1719855635609",
-  storageBucket: "ignite-store-1719855635609.appspot.com",
-  messagingSenderId: "384700029085",
-  appId: "1:384700029085:web:0bbe0fc1479be4c7a5dc74",
-  databaseURL:
-    "https://ignite-store-1719855635609-default-rtdb.firebaseio.com/",
+  apiKey: ".",
+  authDomain: ".",
+  projectId: ".",
+  storageBucket: ".",
+  messagingSenderId: ".",
+  appId: ".",
+  databaseURL: ".",
 };
 
 export default firebaseConfig;
